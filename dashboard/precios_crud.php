2d45e4b8f086bd2f548e0f7f16b1f6155b04ebf0
Jeff Rios <jeff_rios2002@outlook.com>
2023-04-19 11:08:37 -0500

    Se agrega la libreria sweetalert sin conexion a internet

--- dashboard/precios_crud.php
+++ dashboard/precios_crud.php
@@ -167,7 +167,7 @@ if (empty($_SESSION['id'])) {
 
     <!-- Core plugin JavaScript-->
     <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="../vendor/sweetalert2/dist/sweetalert2.all.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="../js/sb-admin-2.min.js"></script>
