10072c821f27faa1ad55446d80b5ea6ec41fa977
Jeff Rios <jeff_rios2002@outlook.com>
2023-04-20 07:18:33 -0500

    Se ha agregado la pantalla de estadisticas

--- dashboard/estadisticas.php
+++ dashboard/estadisticas.php
@@ -18,6 +18,8 @@ if (empty($_SESSION['id'])) {
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/datatables/css/dataTables.bootstrap5.min.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -48,12 +50,11 @@ if (empty($_SESSION['id'])) {
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-2">
-                        <h1 class="h3 mb-0 text-gray-800">Báscula All in One</h1>
+                    <div class="d-sm-flex align-items-center justify-content-between pb-4">
+                        <img src="../img/ico/logo.svg" style="width: 15rem;">
                     </div>
-
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h2 class="h5 mb-0 text-gray-800">Dashboard</h2>
+                        <h2 class="h5 ms-3 mb-0 text-gray-800">Estadísticas</h2>
                     </div>
                     <div class="row">
 
@@ -63,18 +64,6 @@ if (empty($_SESSION['id'])) {
                                 <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h6 class="m-0 font-weight-bold text-primary">Número de usuarios mensuales</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
                                 </div>
                                 <!-- Card Body -->
                                 <div class="card-body">
@@ -130,14 +119,6 @@ if (empty($_SESSION['id'])) {
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-        <div id="layoutAuthentication_footer" class="foot">
-            <?php
-                require("./layout/footer.php");
-            ?>
-        </div>
-            <!-- End of Footer -->
-
         </div>
         <!-- End of Content Wrapper -->
 
@@ -161,15 +142,7 @@ if (empty($_SESSION['id'])) {
 
     <!-- Page level plugins -->
     <script src="../vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="../js/demo/chart-area-demo.js"></script>
-    <script src="../js/demo/chart-pie-demo.js"></script>
     <script src="./js/estadisticas.js"></script>
-
-    <!-- Tarjetas -->
-    <script src="./js/tarjetas.js"></script>
-
 
 </body>
 </html>
--- dashboard/js/estadisticas.js
+++ dashboard/js/estadisticas.js
@@ -2,81 +2,30 @@ const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', '
 const users = document.getElementById('usersArea');
 const materiales = document.getElementById('materialesPie');
 
-$.ajax({
-    url: '../php/materiales.php',
-    type: 'GET',
-    success: function (response) {
-       // console.log(response);
+const data = {
+  labels: meses,
+  datasets: [
+    {
+      label: 'Usuarios',
+      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
     }
-})
+  ]
+};
 
-
-
-
-
-//Grafico de Barras - Usuarios
-const barras = new Chart(users, {
-    type: 'bar',
-  data: {
-    labels: meses,
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
-  },
+const config = {
+  type: 'line',
+  data: data,
   options: {
+    responsive: true,
     maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
     scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
     }
   }
-});
+};
 
-//Grafico de Pie - Materiales
-const pie = new Chart(materiales, {
-})
+new Chart(users, config);
